1f2e3defc409c2baaa029d77cbf19a9f5793c735
Philip Wellnitz [11] <philipwellnitz@gmx.de>
2026-06-18 15:50:17 +0900

    book

--- src/book_2026-02.cpp
+++ src/book_2026-02.cpp
@@ -19,19 +19,14 @@ std::string MACROS_FILENAME = "macros";
 // ---------------------------------------------------------------------------------------------
 
 FILE_SIMPLE( s00_p01, {
-    WITH_PICTURE( p1, { }, doc ) {
-        auto tn = stylized_string{ "T", fragmentco{ 0, 5 }, str_displ_t::NAME };
-        place_string( p1, tn, tikz_point{ 0.0, 0.0 } );
-    }
-
-    constexpr std::string T2       = "DEFGHaJKLMcOPQAB";
-    constexpr std::string P2       = "DEFGHIJKLMNOPQAB";
+    constexpr std::string T2       = "ACGATCTACCG";
+    constexpr std::string P2       = "ACGTGTAACCG";
     const breakpoint_repn BP_P2_T2 = compute_breakpoints( P2, T2 );
 
-    const stylized_string T2_NAME{ T2, std::string{ "T" }, str_displ_t::FRAGMENT };
-    const stylized_string P2_NAME{ P2, std::string{ "P" }, str_displ_t::FRAGMENT };
+    const stylized_string T2_NAME{ T2 };
+    const stylized_string P2_NAME{ P2 };
 
-    add_alignment_picture( doc, BP_P2_T2, T2_NAME, P2_NAME, AT_COMPRESS );
+    add_alignment_picture( doc, BP_P2_T2, T2_NAME, P2_NAME );
 } )
 
 // ---------------------------------------------------------------------------------------------
